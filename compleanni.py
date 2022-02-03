464bc03a5b84fad88cc9a397e4242afce2215057
giumanuz <88536667+giumanuz@users.noreply.github.com>
2022-02-03 22:52:13 +0100

    Insulti e foto completati
     👌🏼

---
+++ compleanni.py
@@ -0,0 +1,14 @@
+import datetime
+import time
+from telegram.ext import CallbackContext
+
+
+def compleanni (context: CallbackContext):
+    while (1):
+
+
+
+
+
+        context.bot.send_message
+        time.sleep(secs= 3600*24)
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import random
+from os import listdir
+
 from compleanni import compleanni
 from telegram import InlineKeyboardMarkup, InlineKeyboardButton, Update
 from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, MessageHandler, Filters, CallbackContext
@@ -10,6 +12,9 @@ class Mine:
     def __init__(self):
         self.__array_list = None
         self.__corrent_player = None
+        self.__lista_culo = []
+        self.__lista_fica = []
+        self.__lista_tette = []
 
         updater = Updater(token="5284256332:AAHv1djfMG6QQTobd-H_jUDpmsjvMgewpNM", use_context=True)
         dispatcher = updater.dispatcher
@@ -20,8 +25,16 @@ class Mine:
 
         dispatcher.add_handler(MessageHandler(Filters.text, self.handler, pass_user_data=True, run_async=True))
 
-        # t = Thread(target= compleanni, args=[updater.])
-        #  t.start()
+        self.__lista_tette = listdir("./Foto/Tette")
+        if '.DS_Store' in self.__lista_tette:
+            self.__lista_tette.remove('.DS_Store')
+        self.__lista_fica = listdir("./Foto/Fica")
+        if '.DS_Store' in self.__lista_fica:
+            self.__lista_fica.remove('.DS_Store')
+        self.__lista_culo = listdir("./Foto/Culo")
+        if '.DS_Store' in self.__lista_culo:
+            self.__lista_culo.remove('.DS_Store')
+
         updater.start_polling()
         updater.idle()
 
@@ -60,9 +73,9 @@ class Mine:
         riga = numero // 3
         colonna = numero % 3
         if self.__corrent_player:
-            self.__array_list[riga][colonna] = InlineKeyboardButton(text="❌", callback_data="-1")
+            self.__array_list[riga][colonna] = InlineKeyboardButton(text="⭕", callback_data="-1")
         else:
-            self.__array_list[riga][colonna] = InlineKeyboardButton(text="⭕️", callback_data="-1")
+            self.__array_list[riga][colonna] = InlineKeyboardButton(text="❌️", callback_data="-1")
         self.__corrent_player = not self.__corrent_player
         update.callback_query.edit_message_reply_markup(reply_markup=InlineKeyboardMarkup(self.__array_list))
 
@@ -80,14 +93,52 @@ class Mine:
         #                                    message_id=step[1],
         #                                    text="Comando non riconosciuto, riprova❗")
 
-        insulti = ["sei più inutile di un preservativo per Dag", "sei proprio un Cioppy"]
+        insulti = ["sei più inutile di un preservativo per Dag", "sei proprio un Cioppy",
+                   "tua madre è una grande tartaruga",
+                   "possano le tue orecchie diventare buchi di culo e defecarti sulle spalle",
+                   "sei come la minchia: sempre tra le palle",
+                   "sei così spaventoso che quando caghi la tua stessa merda dice di fotterti!",
+                   "quando Dio diede l'intelligenza all'umanità tu dov'eri? Al cesso!?",
+                   "sei cosi brutto che chi ti guarda vomita",
+                   "sei cosi ignorante che pure i tuo amici ti stanno lontano",
+                   "sei cosi scemo che guardi pure peppa pig",
+                   "tua madre é peggio di un canestro da basket, gli entrano tutte le palle",
+                   "io non capisco se sei cretino di tuo oppure ci hai studiato per esserlo",
+                   "chiedi a tua mamma se sono frocio",
+                   "sei così brutto che quando sei nato tua mamma ha inviato i biglietti di scuse a tutti",
+                   "fai così schifo che quando ti guardi allo specchio lui si gira dall'altra parte",
+                   "tua mamma ce l'ha così pelosa che per depilarsela deve chiamare la guardia forestale",
+                   "tua madre è come Rowenta.. per chi non si accontenta",
+                   "come ti senti se ti dico che sei solo uno schizzo di sborra di tuo padre?",
+                   "di a tua madre di smettere di cambiare rossetto! Ho il pisello che sembra un arcobaleno!",
+                   "lo sai perchè sulla bandiera della Mongolia c'é la tua faccia? Perché sei il re dei mongoloidi",
+                   "tua mamma è come gli orsi: sempre in cerca di pesce",
+                   "sei cosi brutto ma cosi brutto che tua mamma appena ti ha fatto pensava che fossi uscito dal culo",
+                   "tua madre ha visto più tubi di tutta la saga di Super Mario",
+                   "tua mamma si mette le prugne in culo e ci fa la marmellata", "ti puzza la minchia",
+                   "sei cosi testa di cazzo che quando un'uomo pensa a te puo diventare gay!",
+                   "le tue gambe sono così pelose che per farti la ceretta devi affittare un tagliaerba",
+                   "io ti penso spesso.. ma quando sono al cesso!",
+                   "tua madre è come Buffon, ha sempre palle tra le mani",
+                   "che cos'è una disgrazia? Conoscerti e incontrarti",
+                   "ma tu sei nato male o tuo padre ha usato il preservativo bucato?",
+                   "non ti prendo a calci perché potrebbe piacerti",
+                   "tua mamma e come i mobili dell'ikea: tutti se la montano",
+                   "tua mamma e come un gelato: se la gustano tutti",
+                   "sei così brutto che quando accendi il computer si attiva subito l'antivirus",
+                   "sei così brutto che se i testimoni di geova vengono a casa tua, si chiudono la porta in faccia da soli",
+                   "non ti sputo addosso, perché la mia saliva ti disinfetterebbe",
+                   "sei così brutto che quando vai al mare viene la bassa marea"]
         insieme_fica = {"vagina", "fica", "pisella", "fregna", "figa", "utero", "vulva", "gnegna"}
         insieme_tette = {"tette", "zinne", "seno", "coseno", "poppe", "mammelle", "boops", "boop", "tetta", "zinna"}
         insieme_pene = {"pene", "pisello", "cazzo", "cazzetto", "mazza", "bastone", "arnese", "manganello", "cazzone",
-                        "gingillo"}
+                        "gingillo", "minchia"}
         insieme_negro = {"neg?r\w", "nigga"}
         insieme_culo = {"culo", "lato ?b", "deretano", "fondoschiena", "natiche", "natica", "sedere"}
         lista_cazzi = ["./Foto/2.jpeg", "./Foto/3.jpeg", "./Foto/4.jpeg"]
+        lista_fiche = ["./Foto/5.jpeg", "./Foto/6.jpeg", "./Foto/6.jpeg", "./Foto/7.jpeg", "./Foto/8.jpeg",
+                       "./Foto/9.jpeg", "./Foto/10.jpeg", "./Foto/11.jpeg", "./Foto/12.jpeg", "./Foto/13.jpeg",
+                       "./Foto/14.jpeg", "./Foto/15.jpeg", "./Foto/16.jpeg", "./Foto/17.jpeg"]
 
         if "grazie" in testo:
             context.bot.send_message(chat_id=update.effective_chat.id, text='Ar cazzo')
@@ -104,28 +155,25 @@ class Mine:
                                   photo="http://www.diag.uniroma1.it/~digiamb/website/Files/foto.jpg",
                                   caption="MMM che manzo")
 
-        if "culo" in testo:
+        if any([x in testo for x in insieme_culo]):
             context.bot.sendPhoto(chat_id=update.effective_chat.id,
-                                  photo="https://pbs.twimg.com/profile_images/538777115127455744/NRffC4u8_400x400.jpeg")
-        if "fica" in testo:
+                                  photo=open("./Foto/Culo/" + random.choice(self.__lista_culo), "rb").read())
+        if any([x in testo for x in insieme_fica]):
             context.bot.sendPhoto(chat_id=update.effective_chat.id,
-                                  photo="https://www.lapecorasclera.it/public/immagini_pecora/NATURALE-5.jpg")
-        if any([x in testo for x in insieme_cazzi]):  # list comprehension
+                                  photo=open("./Foto/Fica/" + random.choice(self.__lista_fica), "rb").read())
+        if any([x in testo for x in insieme_pene]):  # list comprehension
             context.bot.sendPhoto(chat_id=update.effective_chat.id,
                                   photo=open(random.choice(lista_cazzi), "rb").read())
-
-        if "tette" in testo:
+        if any([x in testo for x in insieme_tette]):
             context.bot.sendPhoto(chat_id=update.effective_chat.id,
-                                  photo=open("./Foto/1.jpg", "rb").read())
+                                  photo=open("./Foto/Tette/" + random.choice(self.__lista_tette), "rb").read())
         if "botvalo" in testo:
             if "insulta" in testo:
-                lista = testo.split(" ")
-                posizione = lista.index("insulta")
-                nome = lista[posizione + 1]
                 context.bot.send_message(chat_id=update.effective_chat.id,
-                                         text=f'{nome} {random.choice(insulti)}')  # format string
+                                         text=f'Cioppy {random.choice(insulti)}')  # format string
             if "è 30l" in testo:
-                context.bot.send_message(chat_id=update.effective_chat.id, text='Eliminare Cioppy')
+                context.bot.send_message(chat_id=update.effective_chat.id, text='Per penitenza devi scrivere a Lalla')
+
             if "dettu de derni" in testo:
                 context.bot.send_message(chat_id=update.effective_chat.id,
                                          text='Quannu Cesi ha lu cappello, turna \'ndietro e pija l\'umbrello')
