987f03c542e832370ab9407237e0de233b6f23ab
Edoardo Fiocchi <edokyactus@gmail.com>
2022-02-15 22:17:14 +0100

    Modificato snake e ora funziona (male) per i gruppi

--- bot_components/games/snake.py
+++ bot_components/games/snake.py
@@ -1,8 +1,9 @@
 from queue import Queue
 from random import randint
+from typing import Optional
 
-from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import Dispatcher, CallbackQueryHandler, CallbackContext
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update, Message
+from telegram.ext import Dispatcher, CallbackQueryHandler, CallbackContext, Job
 
 
 class CheckableQueue(Queue):
@@ -11,31 +12,19 @@ class CheckableQueue(Queue):
             return item in self.queue
 
 
-class Direction:
+class Snake:
+    active_snake_games = {}
+    # Commands:
+    GO_UP = 0
+    GO_DOWN = 1
+    GO_LEFT = 2
+    GO_RIGHT = 3
+
+    # Directions:
     UP = (0, -1)
     DOWN = (0, 1)
     LEFT = (-1, 0)
     RIGHT = (1, 0)
-    __directions_cw: tuple = (RIGHT, DOWN, LEFT, UP)
-
-    @classmethod
-    def next_cw(cls, cur_direction):
-        cur_dir_index = cls.__directions_cw.index(cur_direction)
-        return cls.__directions_cw[(cur_dir_index + 1) % 4]
-
-    @classmethod
-    def next_ccw(cls, cur_direction):
-        cur_dir_index = cls.__directions_cw.index(cur_direction)
-        return cls.__directions_cw[(cur_dir_index - 1) % 4]
-
-
-class Snake:
-    active_snake_games = {}
-
-    # Commands:
-    STRAIGHT = 0
-    ROTATE_LEFT = 1
-    ROTATE_RIGHT = 2
 
     BLANK_CELL = '⬜'
     SNAKE_CELL = '🟩'
@@ -43,20 +32,29 @@ class Snake:
     DEATH_CELL = '💀'
 
     __SNAKE_GRID_SIZE = 10
+    __SNAKE_GRID_SIZE_GROUP = 7
 
     BUTTONS = InlineKeyboardMarkup(
         [[
-            InlineKeyboardButton(text='↪️', callback_data='snake-1'),
-            InlineKeyboardButton(text='⬆️', callback_data='snake-0'),
-            InlineKeyboardButton(text='↩️', callback_data='snake-2')
-        ]]
-    )
+            InlineKeyboardButton(text='⬆️', callback_data=f'snake-{GO_UP}'),
+        ], [
+            InlineKeyboardButton(text='⬅️', callback_data=f'snake-{GO_LEFT}'),
+            InlineKeyboardButton(text='⬇️', callback_data=f'snake-{GO_DOWN}'),
+            InlineKeyboardButton(text='➡️', callback_data=f'snake-{GO_RIGHT}'),
+        ]])
 
     @classmethod
     def on_menu_entry_click(cls, update: Update, context: CallbackContext):
         chat_id = update.effective_chat.id
         if chat_id not in cls.active_snake_games:
-            snake = Snake(chat_id, context)
+            from telegram import Chat
+            if update.effective_chat.type == Chat.PRIVATE:
+                snake = Snake(chat_id, context, is_group=False)
+                interval = 0.7
+            else:
+                snake = Snake(chat_id, context, is_group=True)
+                interval = 3.05
+                context.bot.send_message(text="Lo snake è lento nei gruppi. Provalo in chat privata!", chat_id=chat_id)
             cls.active_snake_games[chat_id] = snake
             snake_game_message = context.bot.send_message(
                 chat_id=chat_id,
@@ -64,6 +62,8 @@ class Snake:
                 reply_markup=cls.BUTTONS
             )
             snake.set_message(snake_game_message)
+            job = context.job_queue.run_repeating(snake.update, interval=interval)
+            snake.set_job(job)
         else:
             context.bot.send_message(
                 chat_id=chat_id,
@@ -77,29 +77,37 @@ class Snake:
         snake_game: Snake = cls.active_snake_games.get(chat_id, None)
         if snake_game is None or not snake_game.game_active:
             return
-        update.callback_query.answer()
         snake_game.change_direction(command)
-        snake_game.go()
-        cls.__update_message(snake_game, update)
+        update.callback_query.answer()
 
-    def __init__(self, chat_id, context):
-        self.bot = context.bot
+    def __init__(self, chat_id: int, context: CallbackContext, is_group: bool = False):
+        self.context = context
+        self.update_loop_job: Optional[Job] = None
         self.chat_id = chat_id
-        self.message = None
+        self.message: Optional[Message] = None
 
-        self.direction = Direction.RIGHT
+        self.direction = self.RIGHT
         self.snake_queue = CheckableQueue()
-        self.pos = (0, 1)
+        self.pos = (1, 1)
         self.fruit = (3, 3)
-        self.grid = [[self.BLANK_CELL for _ in range(self.__SNAKE_GRID_SIZE)] for _ in range(self.__SNAKE_GRID_SIZE)]
+        self.game_size = self.__SNAKE_GRID_SIZE_GROUP if is_group else self.__SNAKE_GRID_SIZE
+        self.grid = [[self.BLANK_CELL for _ in range(self.game_size)] for _ in range(self.game_size)]
         self.game_active = True
+        self.points = 0
 
         self.snake_queue.put(self.pos)
         self._insert_snake_pos_to_grid(self.pos)
         self._push_fruit_into_grid()
 
+    def set_job(self, job):
+        self.update_loop_job = job
+
     def set_message(self, message):
         self.message = message
+
+    def update(self, _):
+        self.go()
+        self.__update_message()
 
     def go(self):
         self.move_snake()
@@ -111,12 +119,14 @@ class Snake:
         self.pop_snake_position()
 
     def change_direction(self, command):
-        if command == self.STRAIGHT:
-            return
-        elif command == self.ROTATE_LEFT:
-            self.direction = Direction.next_ccw(self.direction)
-        elif command == self.ROTATE_RIGHT:
-            self.direction = Direction.next_cw(self.direction)
+        if command == self.GO_UP:
+            self.direction = self.UP
+        elif command == self.GO_DOWN:
+            self.direction = self.DOWN
+        elif command == self.GO_LEFT:
+            self.direction = self.LEFT
+        elif command == self.GO_RIGHT:
+            self.direction = self.RIGHT
         else:
             raise ValueError("Command not valid")
 
@@ -124,10 +134,11 @@ class Snake:
         return '\n'.join((''.join(x) for x in self.grid))
 
     def lose(self):
+        self.update_loop_job.schedule_removal()
         self.game_active = False
         self.message.edit_reply_markup(reply_markup=None)
         self.active_snake_games.pop(self.chat_id, None)
-        self.bot.send_message(
+        self.context.bot.send_message(
             text="Hai perso!",
             chat_id=self.chat_id
         )
@@ -150,7 +161,7 @@ class Snake:
         self._pop_snake_pos_from_grid(old_pos)
 
     def boundary_hit(self):
-        return any((x < 0 or x >= 10 for x in self.pos))
+        return any((x < 0 or x >= self.game_size for x in self.pos))
 
     def generate_new_fruit(self):
         new_pos = self.__random_position()
@@ -159,20 +170,19 @@ class Snake:
         self.fruit = new_pos
         self._push_fruit_into_grid()
 
-    @classmethod
-    def __random_position(cls) -> tuple[int, int]:
-        return randint(0, cls.__SNAKE_GRID_SIZE - 1), \
-               randint(0, cls.__SNAKE_GRID_SIZE - 1)
+    def __random_position(self) -> tuple[int, int]:
+        return randint(0, self.game_size - 1), \
+               randint(0, self.game_size - 1)
 
-    @classmethod
-    def __update_message(cls, snake_game, update):
-        update.callback_query.edit_message_text(
-            text=snake_game.to_string()
+    def __update_message(self):
+        reply_markup = self.BUTTONS if self.game_active else None
+        self.message.edit_text(
+            text=self.to_string(),
+            reply_markup=reply_markup
         )
-        if snake_game.game_active:
-            update.callback_query.edit_message_reply_markup(
-                reply_markup=cls.BUTTONS
-            )
+
+    def add_points(self):
+        self.points += 10
 
     def _insert_snake_pos_to_grid(self, position: tuple[int, int]):
         self.__set_grid_cell(position, self.SNAKE_CELL)
@@ -193,8 +203,8 @@ class Snake:
 
 def init_snake(dispatcher: Dispatcher):
     dispatcher.add_handler(CallbackQueryHandler(
-        Snake.on_menu_entry_click, pattern="snake-callback"
+        Snake.on_menu_entry_click, pattern="snake-callback", run_async=True
     ))
     dispatcher.add_handler(CallbackQueryHandler(
-        Snake.on_button_click, pattern="snake-[012]"
+        Snake.on_button_click, pattern="snake-[0123]", run_async=True
     ))
