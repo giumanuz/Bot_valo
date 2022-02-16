4198a318d478fee2295d75d056618415dca3712b
Edoardo Fiocchi <edokyactus@gmail.com>
2022-02-16 22:43:16 +0100

    Aggiunti test e ottimizzate risposte

--- tests/conversation_tests.py
+++ tests/conversation_tests.py
@@ -1,7 +1,6 @@
-import logging
 import unittest
 
-from bot_components.foto import Foto, get_set_list
+from bot_components.foto import Foto
 from bot_components.insulti import Insulti
 from bot_components.menu import Menu
 from bot_components.risposte import Risposte
@@ -14,10 +13,6 @@ class ConversationTests(unittest.TestCase):
     bot = MockBot()
     dispatcher = MockDispatcher(bot)
     context = MockContext(dispatcher)
-
-    @classmethod
-    def setUpClass(cls):
-        logging.basicConfig(level=logging.DEBUG)
 
     def setUp(self):
         self.bot.reset_data()
@@ -33,11 +28,7 @@ class ConversationTests(unittest.TestCase):
         self.assertIn(res[0].get('text')[7:], Insulti.lista_insulti)
 
     def test_Foto_ifTriggerWordSent_ShouldSendPhoto(self):
-        for insieme in get_set_list():
-            self.__testPhotoTriggerWord(next(iter(insieme)))
-
-    def __testPhotoTriggerWord(self, word):
-        res = self._send_fake_message_to(Foto, word)
+        res = self._send_fake_message_to(Foto, "mazza")
         self.assertEqual(1, len(res))
         self.assertHasValidPhoto(res)
         self.bot.reset_data()
@@ -46,12 +37,20 @@ class ConversationTests(unittest.TestCase):
         res = self._send_fake_message_to(Foto, "non_trigger")
         self.assertEqual(0, len(res))
 
+    def test_Foto_ifTextContainsTrigger_ShouldSendPhoto(self):
+        res = self._send_fake_message_to(Foto, "test mazza test")
+        self.assertEqual(1, len(res))
+        self.assertHasValidPhoto(res)
+
+    def test_Risposte_ifTextContainsNonExplicitTrigger_ShouldNotSendPhoto(self):
+        res = self._send_fake_message_to(Foto, "testmazzatest")
+        self.assertEqual(0, len(res))
+
     def test_onMenuCommand_ShouldSendMenu(self):
         from telegram.inline.inlinekeyboardmarkup import InlineKeyboardMarkup
         update = MockUpdateFactory.empty()
         Menu.handle_command(update, self.context)
         res = self.bot.result
-
         self.assertEqual(1, len(res))
         self.assertIn('reply_markup', res[0])
         self.assertIsInstance(res[0]['reply_markup'], InlineKeyboardMarkup)
@@ -63,6 +62,10 @@ class ConversationTests(unittest.TestCase):
     def test_Risposte_ifNonTriggerWordSent_ShouldNotReply(self):
         res = self._send_fake_message_to(Risposte, "non_trigger")
         self.assertEqual(0, len(res))
+
+    def test_Risposte_ifTextContainsExplicitTrigger_ShouldReply(self):
+        res = self._send_fake_message_to(Risposte, "this is a test")
+        self.assertEqual(1, len(res))
 
     def _send_fake_message_to(self, cls, text):
         update = MockUpdateFactory.with_message(text)
