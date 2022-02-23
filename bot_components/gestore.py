a9798e88a336753443754ed01d14fa1ec8c9377f
Edoardo Fiocchi <edokyactus@gmail.com>
2022-02-23 19:31:54 +0100

    Il bot non invia foto se l'orario è nella blacklist.

--- bot_components/gestore.py
+++ bot_components/gestore.py
@@ -1,17 +1,45 @@
+import datetime
+import json
+import logging
+
 from telegram import Update
 from telegram.ext import CallbackContext, Dispatcher, MessageHandler, Filters
 
 from bot_components.foto import Foto
 from bot_components.insulti import Insulti
 from bot_components.risposte import Risposte
+from bot_components.utils.os_utils import path_to_text_file
 
-
-def _inoltra_messaggio(update: Update, context: CallbackContext):
-    Foto.handle_message(update, context)
-    Risposte.handle_message(update, context)
-    Insulti.handle_message(update, context)
+week_codes = {0: "monday", 1: "tuesday", 2: "wednesday", 3: "thursday", 4: "friday", 5: "saturday", 6: "sunday"}
+blacklisted_hours: dict[str, list[int]] = None
 
 
 def add_message_handlers(dispatcher: Dispatcher):
+    init_hour_blacklist()
     dispatcher.add_handler(MessageHandler(
         Filters.text, _inoltra_messaggio, pass_user_data=True, run_async=True))
+
+
+def init_hour_blacklist():
+    try:
+        with open(path_to_text_file("schedule_blacklist.json"), "r") as f:
+            global blacklisted_hours
+            blacklisted_hours = json.load(f)
+    except OSError:
+        logging.warning("Errore nell'apertura del file 'schedule_blacklist.json'. "
+                        "Sicuro che si trova in /resources/text_files ?")
+
+
+def _inoltra_messaggio(update: Update, context: CallbackContext):
+    Risposte.handle_message(update, context)
+    Insulti.handle_message(update, context)
+    if not hour_in_blacklist():
+        Foto.handle_message(update, context)
+
+
+def hour_in_blacklist() -> bool:
+    if blacklisted_hours is None:
+        return False
+    forbidden_hour_interval = blacklisted_hours.get(week_codes[datetime.datetime.now().weekday()])
+    hour_now = datetime.datetime.now().hour
+    return forbidden_hour_interval[0] <= hour_now < forbidden_hour_interval[1]
--- bot_components/utils/os_utils.py
+++ bot_components/utils/os_utils.py
@@ -1,10 +1,23 @@
 from os.path import join, dirname
 
 
-def get_absolute_path(path_as_main: str = None):
+def get_absolute_path(path_as_main: str = None) -> bytes:
+    """Prende in input una stringa che rappresenta il percorso al file voluto,
+    partendo dalla directory del main. Per esempio, per ottenere il percorso
+    al file `dichiarazione.json` dentro la cartella `prenotazioni` dentro `resources`,
+    basta mettere in input la stringa \"`/resources/prenotazioni/dichiarazione.json`\"."""
     if path_as_main is None:
         return join(dirname(__file__), "..", "..")
     tokens = path_as_main.strip().split('/')
     if tokens[0] in ('.', ''):
         tokens = tokens[1:]
     return join(dirname(__file__), "..", "..", *tokens)
+
+
+def path_to_text_file(filename: str) -> bytes:
+    """Restituisce il percorso completo a un file partendo dal suo nome.
+
+    **Nota:** Il file deve trovarsi dentro la cartella `resources/text_files`.
+
+    Equivalente di `get_absolute_path(f"/resources/text_files/{filename}")`"""
+    return get_absolute_path(f"resources/text_files/{filename}")
