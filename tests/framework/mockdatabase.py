5fec7b575fb505b3718b462146fd7ad21e52d292
Edoardo Fiocchi <edokyactus@gmail.com>
2022-03-11 17:29:29 +0100

    Modificati test... Che sia tutto finito?

--- tests/framework/mockdatabase.py
+++ tests/framework/mockdatabase.py
@@ -7,6 +7,8 @@ class MockDatabase(Database):
         self.keyword_foto = {}
         self.dict_nicknames = {}
         self.schedule_blacklist = {}
+        self.dict_risposte = {}
+        self.removal_seconds = {}
 
     def load_default_values(self):
         self.lista_insulti = [
@@ -18,30 +20,37 @@ class MockDatabase(Database):
                 "pene", "mazza", "cazzo"
             ]
         }
+        self.dict_nicknames = {"143938748": "Serchio"}
+        self.schedule_blacklist = {}
+        self.dict_risposte = {
+            "grazie": "risposta_test",
+            "ricorsione": "risposta_ricorsione",
+            "ricorsivo": "ALT::ricorsione"
+        }
 
     def register_for_config_changes(self, document: str, callback):
-        pass
+        callback()
 
     def get_lista_insulti(self) -> list[str]:
-        pass
+        return self.lista_insulti
 
     def get_keyword_foto(self) -> dict[str, list[str]]:
-        pass
+        return self.keyword_foto
 
     def get_nicknames(self) -> dict[int, str]:
-        pass
+        return self.dict_nicknames
 
     def get_risposte(self) -> dict[int, any]:
-        pass
+        return self.dict_risposte
 
     def get_schedule_blacklist(self) -> dict[str, list[str]]:
-        pass
+        return self.schedule_blacklist
 
     def get_random_photo(self, category: str) -> bytes:
-        pass
+        return b"random-photo"
 
     def get_chat_removal_seconds(self, chat_id: int, default=5) -> dict:
-        pass
+        return self.removal_seconds.get(str(chat_id), default)
 
     def set_chat_removal_seconds(self, chat_id: int, seconds: float):
-        pass
+        self.removal_seconds[str(chat_id)] = seconds
