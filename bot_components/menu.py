4414fc45d3bb95484b2f9042f29e35ab9f6fd8bb
Edoardo Fiocchi <edokyactus@gmail.com>
2022-03-14 01:30:08 +0100

    [WIP] Aggiunte impostazioni

--- bot_components/menu.py
+++ bot_components/menu.py
@@ -1,12 +1,14 @@
 from telegram import InlineKeyboardMarkup, InlineKeyboardButton, Update
 from telegram.ext import CommandHandler, Dispatcher
 
+from utils.lib_utils import FlowMatrix
+
 
 class Menu:
     __BUTTONS_PER_ROW = 2
     __MENU_TEXT = "〰〰〰〰 MENU 〰〰〰〰"
 
-    buttons_list: list[list[InlineKeyboardButton]] = []
+    buttons_matrix = FlowMatrix(row_length=__BUTTONS_PER_ROW)
 
     @classmethod
     def init(cls, dispatcher: Dispatcher):
@@ -14,20 +16,10 @@ class Menu:
 
     @classmethod
     def show(cls, update: Update, _=None):
-        buttons_markup = InlineKeyboardMarkup(cls.buttons_list)
+        buttons_markup = InlineKeyboardMarkup(cls.buttons_matrix.list)
         update.effective_chat.send_message(cls.__MENU_TEXT, reply_markup=buttons_markup)
 
     @classmethod
     def register_button(cls, name: str, callback: str):
-        cls._create_cell_if_necessary()
         new_button = InlineKeyboardButton(name, callback_data=callback)
-        cls.buttons_list[-1].append(new_button)
-
-    @classmethod
-    def _create_cell_if_necessary(cls):
-        if len(cls.buttons_list) == 0 or cls._list_is_full():
-            cls.buttons_list.append([])
-
-    @classmethod
-    def _list_is_full(cls):
-        return len(cls.buttons_list[-1]) == cls.__BUTTONS_PER_ROW
+        cls.buttons_matrix.append(new_button)
---
+++ utils/lib_utils.py
@@ -0,0 +1,38 @@
+class FlowMatrix:
+    def __init__(self, row_length: int):
+        self._row_width = row_length
+        self.__list: list[list] = []
+
+    @property
+    def list(self):
+        return self.__list
+
+    def append(self, element):
+        self.__create_cell_if_necessary()
+        self.__list[-1].append(element)
+
+    def is_empty(self):
+        return len(self.__list) == 0
+
+    def __create_cell_if_necessary(self):
+        if self.is_empty() or self._row_is_full():
+            self.__list.append([])
+
+    def _row_is_full(self):
+        return len(self.__list[-1]) == self._row_width
+
+    def __len__(self):
+        return len(self.__list)
+
+    @classmethod
+    def from_list(cls, ls: list, row_length: int):
+        from math import ceil
+
+        rl = row_length
+        inner_cells_qnt = ceil(len(ls) / rl)
+        matrix = cls(row_length)
+        matrix.__list = [
+            [x for x in ls[j * rl:(j + 1) * rl]]
+            for j in range(inner_cells_qnt)
+        ]
+        return matrix
